2a8e1ac68773c135159fb81bee81ef9e3cfe5ecb
Natalia <nata22808@mail.ru>
2023-07-19 17:34:54 +0300

    Initial Commit

---
+++ ColorViewSliderSUI/ColorViewSliderSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorViewSliderSUIApp.swift
+//  ColorViewSliderSUI
+//
+//  Created by Наталья Миронова on 19.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorViewSliderSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
